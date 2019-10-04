83e9d884cf5b5712a6856d5b70caa51ed823162d
김성우 <rlatjddn23@gmail.com>
2019-10-04 15:34:54 +0900

    기능 : cors 위치 변경

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,9 +29,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # cors-header
-    'corsheaders',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +42,8 @@ INSTALLED_APPS = [
     # apps
     'api',
 
+    # cors-header
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
