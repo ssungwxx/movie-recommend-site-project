08f08dcea4bf8c990a7e927f3972bb52faff4e92
김성우 <rlatjddn23@gmail.com>
2019-10-04 12:15:33 +0900

    기능 : conflict 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,17 +3,10 @@ module.exports = {
     devServer: {
         proxy: {
             "/api": {
-<<<<<<< HEAD
                 target: "http://52.79.111.26:8000"
             },
             "/static/posters": {
                 target: "http://52.79.111.26:8000"
-=======
-                target: "http://52.79.111.26:8000/"
-            },
-            "/static/posters": {
-                target: "http://52.79.111.26:8000/"
->>>>>>> f19a2fdd81e2a010b7303aa23d76b556fadda99a
             }
         }
     }
