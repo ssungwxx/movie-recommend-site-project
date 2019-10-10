963e2b35c854ac109718c7fe9bfb0f45575dc7ea
김성우 <rlatjddn23@gmail.com>
2019-10-10 09:42:20 +0900

    기능 : jasmine coverage 추가

--- frontend/tests/js/sample.js
+++ frontend/tests/js/sample.js
@@ -1,13 +1,30 @@
-"use strict";
+import Vue from "vue";
+import MovieSearchPage from "pages/MovieSearchPage";
+// import MovieList from "../../src/components/MovieList";
 
-var sayHello = function() {
-    return "hello";
-};
+describe("MovieSearchPage", () => {
+    // 원시 컴포넌트 옵션을 검사합니다.
+    it("has a created hook", () => {
+        expect(typeof MovieSearchPage.created).toBe("function");
+    });
 
-// Test case
-describe("sayHello.js", function() {
-    it('should returns string "hello"', function() {
-        expect(sayHello()).toBe("hello");
-        expect(sayHello()).not.toBe("bye");
+    // 원시 컴포넌트 옵션에서 함수 결과를 테스트합니다.
+    it("sets the correct default data", () => {
+        expect(typeof MovieSearchPage.data).toBe("function");
+        const defaultData = MyComponent.data();
+        expect(defaultData.message).toBe("hello!");
+    });
+
+    // 마운트 할 때 컴포넌트 인스턴스를 검사합니다.
+    it("correctly sets the message when created", () => {
+        const vm = new Vue(MovieSearchPage).$mount();
+        expect(vm.message).toBe("bye!");
+    });
+
+    // 인스턴스를 마운트하고 출력된 결과를 검사합니다.
+    it("renders the correct message", () => {
+        const Constructor = Vue.extend(MovieSearchPage);
+        const vm = new Constructor().$mount();
+        expect(vm.$el.textContent).toBe("bye!");
     });
 });
