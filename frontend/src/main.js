48b586e2150fca109a362413edbe82f7ce57dad9
jichanggyu <sa998464@gmail.com>
2019-10-10 12:19:18 +0900

    update db and add pay system (subscribe)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import store from './store'
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+import IMP from 'vue-iamport'
 
 Vue.config.productionTip = false
+Vue.use(IMP, 'imp20223819')
+Vue.IMP().load()
 
 new Vue({
   vuetify,
