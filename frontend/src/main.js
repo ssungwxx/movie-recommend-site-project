c60cca525b20730552f067d37491d428845dc4a3
jichanggyu <sa998464@gmail.com>
2019-10-10 13:35:32 +0900

    solve error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
+
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
-
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+import IMP from 'vue-iamport'
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import router from './router'
-import store from './store'
-import 'expose-loader?$!expose-loader?jQuery!jquery'
-import IMP from 'vue-iamport'
 
 Vue.config.productionTip = false
 Vue.use(IMP, 'imp20223819')
 Vue.IMP().load()
->>>>>>> data_preprocessing
-
 new Vue({
   Vuetify,
   vuetify,
