2314069657cb1d0cdb36bdbff6195b9a9a4ab717
김성우 <rlatjddn23@gmail.com>
2019-10-10 11:18:53 +0900

    기능 : JS 기본 기능 테스팅

--- frontend/tests/karma.conf.js
+++ frontend/tests/karma.conf.js
@@ -18,18 +18,18 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: { "js/*.js": ["coverage"] },
+        preprocessors: {},
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ["progress", "coverage"],
+        reporters: ["progress"],
 
-        // coverageReporter
-        coverageReporter: {
-            type: "html",
-            dir: "coverage/"
-        },
+        // // coverageReporter
+        // coverageReporter: {
+        //     type: "html",
+        //     dir: "coverage/"
+        // },
 
         // web server port
         port: 9876,
